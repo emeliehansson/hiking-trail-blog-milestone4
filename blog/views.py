89e197b2debe52c23770a6c25c4645ada7e750f7
emeliehansson <emeliehansson_@hotmail.com>
2023-03-20 08:21:53 +0000

    Added a PostList class to views.py Will show the approved posts from the Post Model on the home page. Ordered by newest to oldest post

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import View, CreateView, ListView
+from .models import Post
 
-# Create your views here.
+
+class PostList(ListView):
+    """
+    Takes the Post Model and filters out
+    approved posts and shows them on the home page.
+    """
+    model = Post
+    template_name = 'index.html'
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    paginate_by = 6

ea3c4ec5b30e300201ca8c400ee0e3f91e84d32e
emeliehansson <emeliehansson_@hotmail.com>
2023-03-27 16:53:54 +0000

    Pushing with debug set to false

--- hikingblog/settings.py
+++ hikingblog/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+X_FRAME_OPTIONS: 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['hiking-trail-blog.herokuapp.com', 'localhost']
 
